b7508db402a4b6f9e0c6548078363eab21067a7e
teolostaunau <teo.lostaunau@gmail.com>
2022-07-14 13:37:32 -0500

    Agenda GAS v1.3

--- funciones.js
+++ funciones.js
@@ -24,25 +24,30 @@ function obtenerDatos()
 
 function insertarContacto(contacto, imagen)
 {
-    if(imagen)
-    {
-        let blob = Utilities.newBlob(imagen.datos, imagen.tipo, imagen.nombre);
-        let archivo = CARPETA.createFile(blob);
+    if(imagen) contacto.imagen = guardarImagen(imagen);
 
-        contacto.imagen = CABECERA_URL_IMAGEN_DRIVE + archivo.getId();
-    }
     HOJA.appendRow([contacto.dni,contacto.nombre,contacto.correo,contacto.telefono,contacto.boleto,contacto.imagen]);
+}
+
+
+function modificarContacto(contacto, imagen)
+{
+    if(imagen) contacto.imagen = guardarImagen(imagen);
+
+    let celdas = HOJA.getRange(`A${contacto.fila}:F${contacto.fila}`);
+    celdas.setValues([[contacto.dni,contacto.nombre,contacto.correo,contacto.telefono,contacto.boleto,contacto.imagen]]);
+}
+
+function guardarImagen(imagen)
+{
+    let blob = Utilities.newBlob(imagen.datos, imagen.tipo, imagen.nombre);
+    let archivo = CARPETA.createFile(blob);
+    return CABECERA_URL_IMAGEN_DRIVE + archivo.getId();
 }
 
 function borrarContacto(numFila)
 {
     HOJA.deleteRow(numFila);
-}
-
-function modificarContacto(numFila,datos)
-{
-    let celdas = HOJA.getRange(`A${numFila}:E${numFila}`);
-    celdas.setValues([[datos.dni,datos.nombre,datos.correo,datos.telefono,datos.boleto]]);
 }
 
 function importarContactos()
